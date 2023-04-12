806baee36157911602a1881d6ab4ea4361cea35e
Jack Latrobe <jack@jack-latrobe.com>
2023-04-12 23:38:43 +1000

    Added HTML template to flask

--- langchain-service/main.py
+++ langchain-service/main.py
@@ -1,7 +1,7 @@
 ## Helpchain-Langchain: Microservices components for the langchain component of the GPT solution
 ## File: langchain-service/main.py
 
-from flask import Flask
+from flask import Flask, render_template
 import os
 import socket
 import openai
@@ -12,12 +12,12 @@ app = Flask(__name__)
 def hello():
     try:
         openai.api_key = os.environ.get("OPENAI_KEY")
-        response = openai.Completion.create(model="text-davinci-003", prompt="Write a HTML and CSS page that announces this site is under construction, which includes a random quote of the day in the centre of the page", temperature=0.3, max_tokens=1024)
+        response = openai.Completion.create(model="text-davinci-003", prompt="Write a quote of the day, wrapped in HTML <div> tags", temperature=0.7, max_tokens=200)
         response = response["choices"][0]["text"]
     except Exception as ex:
         response = "Error in GPT API: {}".format(ex)
 
-    return response
+    return render_template('index.html', response)
 
 @app.route("/health")
 def healthcheck():
