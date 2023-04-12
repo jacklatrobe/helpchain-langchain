7942aa81792d5a14425243ca8409f133934d2299
Jack Latrobe <jack@jack-latrobe.com>
2023-04-12 15:51:14 +1000

    Fixed secrets, added health check

--- helloworld/main.py
+++ helloworld/main.py
@@ -8,10 +8,14 @@ import socket
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     html = """Hello {name}!
     Hostname: {hostname}"""
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
+
+@app.route("/health")
+def healthcheck():
+    return "OK"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
